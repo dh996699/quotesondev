1bcd9b2de1f3f3ea2ff0d6280467e55b857a543b
David Huynh <dh99.66.99@gmail.com>
2019-08-31 13:06:53 -0700

    remove page-about.php and move the contains to page.php, addjust the quote to fix on about page

--- page.php
+++ page.php
@@ -12,7 +12,22 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<article class="about-page-container" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="about-entry-header">
+						<?php the_title( '<h1 class="about-entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
+
+					<div class="about-entry-content">
+						<p><?php the_content(); ?></p>
+						<?php
+						wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+						'after'  => '</div>',
+						) );
+						?>
+					</div><!-- .entry-content -->
+
+				</article><!-- #post-## -->
 
 			<?php endwhile; // End of the loop. ?>
 
