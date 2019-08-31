906de978498ee0bcfa0b69eb839e9406338c1e42
David Huynh <dh99.66.99@gmail.com>
2019-08-31 13:41:50 -0700

    modify 404.php and searchform.php to change the search button

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
+			<section class="error-404-not-found">
 				<header class="error-page-header">
 					<h1 class="error-page-title"><?php echo esc_html( 'Oops!' ); ?></h1>
 				</header><!-- .page-header -->
@@ -21,7 +21,6 @@ get_header(); ?>
 					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try a search?' ); ?></p>
 
 					<?php get_search_form(); ?>
-					<?php '<i class="fas fa-search"></i>'?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
