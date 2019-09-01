945131669e49f7f1620f4f0702a5725810267b84
David Huynh <dh99.66.99@gmail.com>
2019-09-01 10:19:28 -0700

    update page 404.php with the icon and search bar in the right location

--- searchform.php
+++ searchform.php
@@ -1,11 +1,9 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<fieldset class="fieldset-container">
-		<div>
+	<fieldset>
+		<div class="fieldset-container">
 		<label>
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
-		</div>
-		<div>
 		<button class="search-submit">
 			<?php echo '<i class="fa fa-search"></i>'?>
 		</button>
